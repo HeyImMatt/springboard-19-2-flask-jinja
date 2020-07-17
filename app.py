a83e32c60b40807fa364ce0bb6c044ccfbd077b9
heyimmatt <mrmcfarland@gmail.com>
2020-07-17 15:27:38 -0700

    Import stories helper and use story prompts to generate form

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
 # from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'key-1234'
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+    return render_template('form.html', prompts=story.prompts)
